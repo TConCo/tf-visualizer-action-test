27479388d689a2c0f023f3594b18ee3746b79249
Fatih Tokus <fatih.tokus@gmail.com>
2023-01-14 14:03:44 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,18 @@
-resource "aws_security_group_rule" "my-rule" {
+variable "template" {
+  type = string
+  default = "0.0.0.0/0"
+}
+
+resource "aws_security_group_rule" "my-rule1" {
+    type = "ingress"
+    cidr_blocks = [var.template]
+}
+
+resource "aws_security_group_rule" "my-rule2" {
     type = "ingress"
     cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-ingress-sgr
 }
+
 resource "aws_s3_bucket" "my-bucket" {
   bucket = "foobar"
   acl    = "private"
