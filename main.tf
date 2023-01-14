f8762550fc97951498a8c49d67e8c631b7f99a11
Fatih Tokus <fatih.tokus@gmail.com>
2023-01-14 14:12:58 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,12 +5,12 @@ variable "template" {
 
 resource "aws_security_group_rule" "my-rule1" {
     type = "ingress"
-    cidr_blocks = [var.template]
+    cidr_blocks = [var.template]#tfsec:ignore:aws-vpc-no-public-ingress-sgr:exp:2023-01-13
 }
 
 resource "aws_security_group_rule" "my-rule2" {
     type = "ingress"
-    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-ingress-sgr
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-ingress-sgr:exp:2023-01-13
 }
 
 resource "aws_s3_bucket" "my-bucket" {
